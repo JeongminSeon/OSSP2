b035e481afed435858c337312b40376b4d5a7b2e
JeongminSeon <46957316+JeongminSeon@users.noreply.github.com>
2022-11-16 23:49:36 +0900

    mcts, tictactoe

---
+++ Jeongmin-Working/TicTacToe.py
@@ -0,0 +1,226 @@
+
+from copy import deepcopy
+from mcts1 import *
+
+# Tic Tac Toe board class
+class Board():
+    # create constructor (init board class instance)
+    def __init__(self, board=None):
+        # define players
+        self.player_1 = 'x'
+        self.player_2 = 'o'
+        self.empty_square = '.'
+
+        # define board position
+        self.position = {}
+
+        # init (reset) board
+        self.init_board()
+
+        # create a copy of a previous board state if available
+        if board is not None:
+            self.__dict__ = deepcopy(board.__dict__)
+
+    # init (reset) board
+    def init_board(self):
+        # loop over board rows
+        for row in range(3):
+            # loop over board columns
+            for col in range(3):
+                # set every board square to empty square
+                self.position[row, col] = self.empty_square
+
+    # make move
+    # col : x cordinate, row -> y cordinate
+    # this is more human-friendly
+
+    # 사용자 입력을 받은 뒤 x,y -> swap
+    def make_move(self, row, col):
+        # create new board instance that inherits from the current state
+        board = Board(self)
+
+        # make move
+        board.position[row, col] = self.player_1
+
+        # swap players
+        (board.player_1, board.player_2) = (board.player_2, board.player_1)
+
+        # return new board state
+        return board
+
+    def is_draw(self):
+       
+        for row, col in self.position:
+            # empty square is available
+            if self.position[row, col] == self.empty_square:
+                # this is not a draw
+                return False
+
+        return True
+
+    def is_win(self):
+
+        # vertical sequence detection
+        for col in range(3):
+            # define winning sequence list
+            winning_sequence = []
+
+            for row in range(3):
+                # if found same next element in a row
+                if self.position[row, col] == self.player_2:
+                    # update winning sequence
+                    winning_sequence.append((row, col))
+                # if we have 3 elements in the row
+                if len(winning_sequence) == 3:
+                    # return the game is won state
+                    return True
+
+       
+        # horizontal sequence detection
+        for row in range(3):
+            # define winning sequence list
+            winning_sequence = []
+
+            for col in range(3):
+                if self.position[row, col] == self.player_2:
+                    # update winning sequence
+                    winning_sequence.append((row, col))
+                # if we have 3 elements in the row
+                if len(winning_sequence) == 3:
+                    # return the game is won state
+                    return True
+
+        # 1st diagnoal sequence detection
+
+        winning_sequence = []
+
+        for row in range(3):
+            col = row
+
+            # define winning sequence list
+            if self.position[row, col] == self.player_2:
+                # update winning sequence
+                winning_sequence.append((row, col))
+            # if we have 3 elements in the row
+            if len(winning_sequence) == 3:
+                # return the game is won state
+                return True
+
+        # 2nd '' 
+        winning_sequence = []
+
+        for row in range(3):
+            col = 3 - row - 1
+
+            # define winning sequence list
+            if self.position[row, col] == self.player_2:
+                # update winning sequence
+                winning_sequence.append((row, col))
+            # if we have 3 elements in the row
+            if len(winning_sequence) == 3:
+                # return the game is won state
+                return True
+
+        return False
+
+    def generate_states(self):
+        # define states list (move list - list of available actions to cosider)
+        actions = []
+
+        for row in range(3):
+            for col in range(3):
+                if self.position[row,col] == self.empty_square:
+                    actions.append(self.make_move(row,col))
+
+        # return the list of available actions (board class instances)
+        # these actions must be iterable!!!!!
+        return actions
+
+    # main game loop
+    def game_loop(self):
+        print('\n틱택톡 게임 ')
+        print(' move를 입력하기 예) 1,1 format [x][y] 종료 : exit ')
+
+        print(self)
+
+        # create MCTS instance
+        mcts = MCTS()
+
+        # game loop
+        while True:
+            # user input
+            user_input = input('> ')
+
+            if user_input == 'exit': break
+            if user_input == '': continue 
+            
+            try:
+                # parse user input (move format [col, row]: 1,2)
+                row = int(user_input.split(',')[1]) - 1
+                col = int(user_input.split(',')[0]) - 1
+
+                # check move 
+                if self.position[row, col] != self.empty_square:
+                    print(' Illegal move!')
+                    continue
+                
+                # make move on baord (사람 입력을 받아서 움직임)
+                self = self.make_move(row, col)
+                
+                # serach for best_move
+                best_move = mcts.search(self)
+
+                # legal moves available
+                try:
+                    # make AI move 
+                    self = best_move.board
+
+                # game over
+                except:
+                    pass
+
+                print(self)
+
+                # check the game state
+                if self.is_win():
+                    print('player %s 가 게임을 이겼습니다 !!' % self.player_2)
+                    break
+
+                elif self.is_draw():
+                    print(' Game is drawn! ')
+                    break
+
+            except Exception as e:
+                print('Error : ', e)
+                print('Illegal move')
+                print(' Move foramt [x,y] : 1, 2')
+
+    # print board state
+    def __str__(self):
+        # define board string representation
+        board_string = ''
+
+        for row in range(3):
+            for col in range(3):
+                # 문자열 대입 연산자
+                board_string += ' %s' % self.position[row, col]
+
+            board_string += '\n'
+
+        # prepand side to move
+        if self.player_1 == 'x':
+            board_string = '\n-----------------\n "x" to move:\n-----------------\n\n' + board_string
+
+        elif self.player_1 == 'o':
+            board_string = '\n-----------------\n "o" to move:\n-----------------\n\n' + board_string
+
+        return board_string
+
+
+# main driver
+if __name__ == '__main__':
+    # create board instance
+    board = Board()
+    
+    # start game loop
+    board.game_loop()
---
+++ Jeongmin-Working/mcts.py
@@ -0,0 +1,159 @@
+
+# packages
+import math
+import random 
+
+# tree node class definition
+class TreeNode():
+    # class constructor (create tree node class instance)
+
+    def __init__(self, board, parent):
+        # init board state
+        self.board = board
+
+        # init is node terminal (flag)
+        if self.board.is_win() or self.board.is_draw():
+            # we have a terminal node
+            self.is_terminal = True
+        
+        else:
+            self.is_terminal = False
+        
+        # set is fully expanded flag
+        self.is_fully_expanded = self.is_terminal
+ 
+        # init parent node if available
+        self.parent = parent
+        # init the number of node visits
+        self.visits = 0
+
+        # init the total score of the node
+        self.score = 0
+
+        # init current node's children
+        self.children = {}
+
+# MCTS class definition
+class MCTS() :
+    # search for the best move in the current position
+    def search(self, initial_state):
+        # create root node
+        self.root = TreeNode(initial_state, None)
+
+        for _ in range(1000):
+            # select a node (selection)
+            node = self.select(self.root)
+            
+            # score current node (simiulation)
+            score = self.rollout(node.board)
+
+            # backpropagate results
+            self.backpropagate(node, score)
+
+        # pick up the best move in the current position
+        try:
+            return self.get_best_move(self.root, 0)
+        
+        except:
+            pass
+    
+    # select most promising node
+    def select(self, node):
+        # make sure that the node is not terminal state
+        while not node.is_terminal:
+            # fully exapnded
+            # best_move 함수를 통해 가장 유망한 자식 노드를 받을 수 있음
+            if node.is_fully_expanded:
+                node = self.get_best_move(node, 2)
+
+            # not fully expanded
+            else:
+                # 완전 확장 될때에만 계산 가능 -> 노드 확장
+                return self.expand(node)
+        
+        # fully exapnded -> 자식 노드 반환
+        # not -> 새로 확장된 노드 반환
+        return node
+
+    def expand(self, node):
+        # generate legal states (moves)
+        states = node.board.generate_states()
+
+        # loop over generated states (movevs)
+        for state in states :
+            # 현재 상태(move)가 자식노드에 존재하면 안됨
+            # stringfy해서 dic에서 key로 사용 -> 중복 X
+            if str(state.position) not in node.children:
+                # create a new node
+                new_node = TreeNode(state, node)
+
+                # 자식 노드를 부모 노드 children dict(list) 에 추가
+                node.children[str(state.position)] = new_node
+
+                # no more legal action 
+                if len(states) == len(node.children) :
+                    node.is_fully_expanded = True
+                
+                # 새로 만든 노드 반환
+                return new_node
+
+        # debugging
+        print('여기 오면 이상한 것')
+
+    # simulate the game via making random moves until reach end of the game
+    def rollout(self, board) : 
+        # terminal state에 도달할 때까지 랜덤 액션(move)
+        while not board.is_win():
+            try:
+                board = random.choice(board.generate_states())
+
+            # no moves available
+            except:
+                return 0
+
+        if board.player_2 == 'x' : return 1
+        elif board.player_2 == 'o' : return -1
+
+
+    # backpropagate the number of visits and score up to the root node
+    def backpropagate(self, node, score):
+        while node is not None:
+            # update node's visits
+            node.visits += 1
+
+            # update node's score
+            node.score += score
+
+            # set node to parent
+            # 노드 한단계 업 -> 부모 노드까지 올라감.
+            node = node.parent
+
+
+    # select the best node basing on UCB1 formula
+    def get_best_move(self, node, exploration_constant):
+        # define best score & best moves
+        # 음의 무한대 반환
+        best_score = float('-inf')
+        best_moves = []
+        
+        # loop over child nodes
+        for child_node in node.children.values():
+            # define current player
+            if child_node.board.player_2 == 'x': current_player = 1
+            elif child_node.board.player_2 == 'o': current_player = -1
+            
+            # get move score using UCT formula
+            move_score = current_player * child_node.score / child_node.visits + exploration_constant * math.sqrt(math.log(node.visits / child_node.visits))                                        
+            
+            # better move has been found
+            if move_score > best_score:
+                best_score = move_score
+                best_moves = [child_node]
+            
+            # found as good move as already available
+            elif move_score == best_score:
+                best_moves.append(child_node)
+            
+        # return one of the best moves randomly
+        return random.choice(best_moves)
+        
