aa7d835352733057d4a6401221bda6dbe0011de0
chanhale <chan771788@gmail.com>
2022-12-20 12:43:58 +0900

    Heuristic agent

--- ChanhaleeWorking/QuoridorGUI.py
+++ ChanhaleeWorking/QuoridorGUI.py
@@ -3,8 +3,6 @@ import pygame
 import time
 import sys
 from pygame.locals import *
-from QuoridorEnv import QuoridorEnv
-from DumbAgent import DumbAgent
 
 #  QuoridorEnv와 동기화가 필요한 항목들
 AGENT_1 = 100
@@ -583,16 +581,16 @@ class QuoridorGUI():
         self.game_done = done
 
 
-q = QuoridorEnv(width=9, value_mode=9)
-agent_1 = q.register_agent()
-agent_2 = q.register_agent()
-dumb = DumbAgent(q, agent_2)
-dumb2 = DumbAgent(q, agent_1)
-print(q.get_legal_action(q.get_state(agent_1)))
-q.render(agent_1)
-# g = QuoridorGUI(q, agent_num1=agent_1, agent_num2=agent_2,
-#                 auto_pilot1=None, auto_pilot2=None)
-g = QuoridorGUI(q, agent_num1=agent_1,
-                auto_pilot1=dumb, auto_pilot2=None)
-# g = QuoridorGUI(q, auto_pilot1=dumb, auto_pilot2=dumb2)
-g.startGame()
+# q = QuoridorEnv(width=9, value_mode=9)
+# agent_1 = q.register_agent()
+# agent_2 = q.register_agent()
+# dumb = DumbAgent(q, agent_2)
+# dumb2 = DumbAgent(q, agent_1)
+# print(q.get_legal_action(q.get_state(agent_1)))
+# q.render(agent_1)
+# # g = QuoridorGUI(q, agent_num1=agent_1, agent_num2=agent_2,
+# #                 auto_pilot1=None, auto_pilot2=None)
+# g = QuoridorGUI(q, agent_num1=agent_1,
+#                 auto_pilot1=dumb, auto_pilot2=None)
+# # g = QuoridorGUI(q, auto_pilot1=dumb, auto_pilot2=dumb2)
+# g.startGame()
